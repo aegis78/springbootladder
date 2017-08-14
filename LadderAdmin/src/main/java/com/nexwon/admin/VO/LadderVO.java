f0a5e7cc5ddefbd58b176f4240cf715a27995324
yiseungju <yiseungju@192.168.0.86>
2017-08-14 14:54:26 +0900

    VO Append

---
+++ LadderAdmin/src/main/java/com/nexwon/admin/VO/LadderVO.java
@@ -0,0 +1,43 @@
+package com.nexwon.admin.VO;
+
+public class LadderVO {
+	
+	private int idx;
+	private int g_idx;
+	private String g_info;
+	private String g_time;
+	private String date;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public int getG_idx() {
+		return g_idx;
+	}
+	public void setG_idx(int g_idx) {
+		this.g_idx = g_idx;
+	}
+	public String getG_info() {
+		return g_info;
+	}
+	public void setG_info(String g_info) {
+		this.g_info = g_info;
+	}
+	public String getG_time() {
+		return g_time;
+	}
+	public void setG_time(String g_time) {
+		this.g_time = g_time;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+}
--- LadderAdmin/src/main/java/com/nexwon/admin/dao/LadderDAO.java
+++ LadderAdmin/src/main/java/com/nexwon/admin/dao/LadderDAO.java
@@ -1,10 +1,16 @@
 package com.nexwon.admin.dao;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.nexwon.admin.VO.LadderVO;
+import com.nexwon.admin.VO.ParamVO;
+
 
 @Repository
 public class LadderDAO {
@@ -15,6 +21,11 @@ public class LadderDAO {
 	
 	public String getCurrentDateTime() {
         return sqlSession.selectOne("ladder.getCurrentDateTime");
+    }
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public ArrayList<LadderVO> getLadderList(ParamVO paramVO) {		
+    		return (ArrayList) sqlSession.selectList("ladder.ladderList", paramVO);
     }
 	
 
